08973ce839a19c8df156a719a5717fcf744159b1
Nivas <nivasduggineni105gmail.com>
2025-11-20 10:15:36 +0530

    first commit

---
+++ class work/conditional_statements.py
@@ -0,0 +1,8 @@
+products = ['laptop',"mouse","phone",'keyboard','charger','speakar']
+stocks=[100,200,300,400,0,6]
+print("*"*30)
+network = True
+if network and products:
+    if product in products:
+
+     print("product not found")
